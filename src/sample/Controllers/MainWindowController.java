6cd5d51d54044479afe37351adaecef9be5ab141
Paweł A <ruri0000@gmail.com>
2018-01-13 17:33:07 +0100

    Rename of main window

--- src/sample/Controllers/MainWindowController.java
+++ src/sample/Controllers/MainWindowController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class SampleController {
+public class MainWindowController {
 
     private CardPack cardPack = new CardPack();
     List<Card> playerCards = new ArrayList<Card>();
