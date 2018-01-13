bd29e028bef138604a9131974bf83c0c483e7c66
Paweł A <ruri0000@gmail.com>
2018-01-13 21:45:52 +0100

    Added dealer drawing cards to soft cap of 17

--- src/sample/Controllers/MainWindowController.java
+++ src/sample/Controllers/MainWindowController.java
@@ -13,10 +13,14 @@ import java.util.List;
 public class MainWindowController {
 
     private CardPack cardPack = new CardPack();
-    List<Card> playerCards = new ArrayList<Card>();
+    List<Card> playerCards = new ArrayList<>();
+    List<Card> dealerCards = new ArrayList<>();
 
     @FXML
     private HBox playerCardContainer;
+
+    @FXML
+    private HBox dealerCardContainer;
 
     private Label createNewCard() {
         Label newLabel = new Label();
@@ -26,24 +30,38 @@ public class MainWindowController {
         return newLabel;
     }
 
-    private void addCardToScene(String text) {
-        Label newLabel = createNewCard();
-        newLabel.setText(text);
-        playerCardContainer.getChildren().add(newLabel);
+    private void dealerDrawCard() {
+        Card newCard = cardPack.pickCard();
+        if (newCard == null) {
+            System.out.println("Pack is out of cards!");
+            return;
+        }
+        dealerCards.add(newCard);
+        addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType() + "s", dealerCardContainer);
+
+        int currentPoints = countPoints(dealerCards);
+        System.out.println("Dealer picked a" + " " + newCard.getValue() + " of " + newCard.getType() + "s worth " + newCard.getNumericalValue() + ", with total of " + currentPoints);
     }
 
-    private int countPlayerPoints() {
+    private void addCardToScene(String text, HBox container) {
+        Label newLabel = createNewCard();
+        newLabel.setText(text);
+        container.getChildren().add(newLabel);
+    }
+
+    private int countPoints(List<Card> cardList) {
         int value = 0;
-        for (Card card : playerCards) {
+        for (Card card : cardList) {
             value += card.getNumericalValue();
         }
 
         return value;
     }
 
-    public void pickCardButtonClicked() {
+    @FXML
+    private void pickCardButtonClicked() {
 
-        int currentPoints = countPlayerPoints();
+        int currentPoints = countPoints(playerCards);
         if (currentPoints > 21) {
             System.out.println("Player lost!");
             return;
@@ -56,13 +74,52 @@ public class MainWindowController {
             return;
         }
         playerCards.add(newCard);
-        addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType() + "s");
-        currentPoints = countPlayerPoints();
+        addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType() + "s", playerCardContainer);
+        currentPoints = countPoints(playerCards);
         System.out.println("Player picked a" + " " + newCard.getValue() + " of " + newCard.getType() + "s worth " + newCard.getNumericalValue() + ", with total of " + currentPoints);
 
-        if (currentPoints > 21)
-        {
+        if (currentPoints > 21) {
             System.out.println("BUSTED!");
         }
     }
+
+    @FXML
+    private void standButtonClicked() {
+        dealerDrawCards();
+    }
+
+    @FXML
+    private void startNewGameButtonClicked() {
+        cardPack = new CardPack();
+
+        dealerCardContainer.getChildren().removeAll();
+        dealerCardContainer.getChildren().clear();
+        playerCardContainer.getChildren().removeAll();
+        playerCardContainer.getChildren().clear();
+
+        dealerCards = new ArrayList<>();
+        playerCards = new ArrayList<>();
+
+        dealerDrawCard();
+    }
+
+    private void dealerDrawCards() {
+        int dealerPoints;
+        while ((dealerPoints = countPoints(dealerCards)) < 17) {
+            Card newCard = cardPack.pickCard();
+            dealerCards.add(newCard);
+            addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType(), dealerCardContainer);
+        }
+
+        int playerPoints = countPoints(playerCards);
+
+        if (dealerPoints > playerPoints)
+            System.out.println("Dealer won! " + dealerPoints + " > " + playerPoints);
+        else if (dealerPoints == playerPoints)
+            System.out.println("DRAW!");
+        else
+            System.out.println("Player won! "  + playerPoints + " > " + dealerPoints);
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Views/MainWindowView.fxml"));
-        //todo
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Blackjack");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
