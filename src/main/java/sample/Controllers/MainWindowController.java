85efb7aa46fedf7230302cea87a3d937bf7d6329
Paweł A <ruri0000@gmail.com>
2018-01-30 16:47:59 +0100

    Refactored game logic to separated class

--- src/main/java/sample/Controllers/MainWindowController.java
+++ src/main/java/sample/Controllers/MainWindowController.java
@@ -6,17 +6,13 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import main.java.sample.Dao.DBStatsInserter;
-import main.java.sample.Models.Card;
-import main.java.sample.Models.CardPack;
+import main.java.sample.GameLogic.Game;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainWindowController {
 
-    private CardPack cardPack = new CardPack();
-    List<Card> playerCards = new ArrayList<>();
-    List<Card> dealerCards = new ArrayList<>();
+    private Game game = new Game();
 
     // result overlay
     @FXML
@@ -40,48 +36,37 @@ public class MainWindowController {
 
     @FXML
     private void hitButtonClicked() {
-        List<Integer> playerPoints = countPoints(playerCards);
-        int lowestPlayerPoints = playerPoints.get(0);
-
-        // if player has more than 19 points and draws new card
-        if (lowestPlayerPoints > 19) {
-            gameEnd("Dealer");
+        if (game.getGameResult().equals("Player lost")) {
+            gameEnd("Player lost");
             return;
         }
 
-        Card newCard = cardPack.pickCard();
-        if (newCard == null) {
-            System.out.println("Pack is out of cards!");
-            return;
-        }
-        playerCards.add(newCard);
-        addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType() + "s", playerCardContainer);
+        String cardText = game.hit();
+        addCardToScene(cardText, playerCardContainer);
 
-        lowestPlayerPoints = countPoints(playerCards).get(0);
-        if (lowestPlayerPoints > 21) {
-            gameEnd("Dealer");
+        if (game.getGameResult().equals("Player lost")) {
+            gameEnd("Player lost");
         }
     }
 
     @FXML
     private void standButtonClicked() {
-        enableHitStandButtons(true);
-        dealerDrawCards();
+        disableHitStandButtons(true);
+
+        List<String> cardList = game.dealerDrawCards();
+        for (String cardText : cardList) {
+            addCardToScene(cardText, dealerCardContainer);
+        }
+
+        checkWinner();
     }
 
     @FXML
     private void startNewGameButtonClicked() {
-        enableHitStandButtons(false);
+        game.startNewGame();
 
-        cardPack = new CardPack();
-
-        dealerCardContainer.getChildren().removeAll();
-        dealerCardContainer.getChildren().clear();
-        playerCardContainer.getChildren().removeAll();
-        playerCardContainer.getChildren().clear();
-
-        dealerCards = new ArrayList<>();
-        playerCards = new ArrayList<>();
+        disableHitStandButtons(false);
+        removeCardsFromScreen();
 
         dealerPickCardFromDeck();
     }
@@ -92,57 +77,34 @@ public class MainWindowController {
         resultMessagePane.setDisable(true);
     }
 
-    private void checkWinner(int maxDealerPoints) {
-        List<Integer> playerPointsList = countPoints(playerCards);
-        int playerPoints = 0;
+    private void showResultMessage() {
+        disableHitStandButtons(true);
+        resultMessagePane.setVisible(true);
+        resultMessagePane.setDisable(false);
+    }
 
-        // playerPoints is sorted list with highest value at the end
-        for (int i = playerPointsList.size() - 1; i >= 0; i--) {
-            playerPoints = playerPointsList.get(i);
+    private void removeCardsFromScreen() {
+        dealerCardContainer.getChildren().removeAll();
+        dealerCardContainer.getChildren().clear();
 
-            if (playerPoints <= 21)
-                break;
-        }
+        playerCardContainer.getChildren().removeAll();
+        playerCardContainer.getChildren().clear();
+    }
 
-        String winner = "";
-        if (maxDealerPoints == playerPoints) {
-            System.out.println("DRAW! " + maxDealerPoints + " = " + playerPoints);
-            winner = "draw";
-        } else if (21 - maxDealerPoints < 21 - playerPoints && maxDealerPoints <= 21) {
-            System.out.println("Dealer won! " + maxDealerPoints + " > " + playerPoints);
-            winner = "dealer";
-        } else {
-            System.out.println("Player won! " + maxDealerPoints + " < " + playerPoints);
-            winner = "player";
-        }
+    private void checkWinner() {
 
-        saveResultsIntoDatabase(playerPoints, maxDealerPoints, winner);
-        gameEnd(winner);
+        game.calculateWinner();
+        String gameResult = game.getGameResult();
+        gameEnd(gameResult);
     }
 
     private void gameEnd(String winner) {
-        resultMessagePane.setVisible(true);
-        resultMessagePane.setDisable(false);
-
-        if (winner == "draw")
+        if (winner.equals("draw"))
             resultLabel.setText("DRAW!");
         else
-            resultLabel.setText(winner + " won!");
-    }
+            resultLabel.setText(winner + "!");
 
-    private void dealerDrawCards() {
-        List<Integer> dealerPoints = countPoints(dealerCards);
-        int maxDealerPoints = dealerPoints.get(dealerPoints.size() - 1);
-
-        while (maxDealerPoints < 17) {
-            Card newCard = cardPack.pickCard();
-            dealerCards.add(newCard);
-            addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType(), dealerCardContainer);
-
-            maxDealerPoints = countPoints(dealerCards).get(dealerPoints.size() - 1);
-        }
-
-        checkWinner(maxDealerPoints);
+        showResultMessage();
     }
 
     private Label createCardLabel() {
@@ -154,13 +116,12 @@ public class MainWindowController {
     }
 
     private void dealerPickCardFromDeck() {
-        Card newCard = cardPack.pickCard();
-        if (newCard == null) {
-            System.out.println("Pack is out of cards!");
-            return;
+        String cardText = game.dealerPickCard();
+        addCardToScene(cardText, dealerCardContainer);
+
+        if (game.getGameResult().equals("Dealer lost")) {
+            gameEnd("Player");
         }
-        dealerCards.add(newCard);
-        addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType() + "s", dealerCardContainer);
     }
 
     private void addCardToScene(String text, HBox container) {
@@ -169,29 +130,7 @@ public class MainWindowController {
         container.getChildren().add(newLabel);
     }
 
-    private List<Integer> countPoints(List<Card> cardList) {
-
-        int value = 0;
-        int aces = 0;
-        for (Card card : cardList) {
-            value += card.getNumericalValue();
-            if (card.getValue() == "Ace") {
-                aces++;
-            }
-        }
-
-        List<Integer> possiblePoints = new ArrayList<>();
-        possiblePoints.add(value);
-
-        // count points for each ace combination
-        for (int i = 1; i <= aces; i++) {
-            possiblePoints.add(value - i + i * 11);
-        }
-
-        return possiblePoints;
-    }
-
-    private void enableHitStandButtons(Boolean lock) {
+    private void disableHitStandButtons(Boolean lock) {
         buttonHit.setDisable(lock);
         buttonStand.setDisable(lock);
     }
---
+++ src/main/java/sample/GameLogic/Game.java
@@ -0,0 +1,129 @@
+package main.java.sample.GameLogic;
+
+import main.java.sample.Models.Card;
+import main.java.sample.Models.CardPack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private CardPack cardPack = new CardPack();
+    List<Card> playerCards = new ArrayList<>();
+    List<Card> dealerCards = new ArrayList<>();
+
+    public String getGameResult() {
+        return gameResult;
+    }
+
+    private String gameResult = "";
+
+    public String hit() {
+        Card newCard = cardPack.pickCard();
+        playerCards.add(newCard);
+
+        if (checkIfPlayerLost()) {
+            gameResult = "Player lost";
+        }
+
+        return newCard.getValue() + "\nof\n" + newCard.getType() + "s";
+    }
+
+    private List<Integer> countPoints(List<Card> cardList) {
+
+        int value = 0;
+        int aces = 0;
+        for (Card card : cardList) {
+            value += card.getNumericalValue();
+            if (card.getValue() == "Ace") {
+                aces++;
+            }
+        }
+
+        List<Integer> possiblePoints = new ArrayList<>();
+        possiblePoints.add(value);
+
+        // count points for each ace combination
+        for (int i = 1; i <= aces; i++) {
+            possiblePoints.add(value - i + i * 11);
+        }
+
+        return possiblePoints;
+    }
+
+    private boolean checkIfPlayerLost() {
+        int playerPoints = countPoints(playerCards).get(0);
+        if (playerPoints > 21) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkIfDealerLost() {
+        int dealerPoints = countPoints(dealerCards).get(0);
+        if (dealerPoints > 21)
+            return true;
+
+        return false;
+    }
+
+    public void startNewGame() {
+        gameResult = "";
+        playerCards = new ArrayList<>();
+        dealerCards = new ArrayList<>();
+    }
+
+    public String dealerPickCard() {
+        Card newCard = cardPack.pickCard();
+        dealerCards.add(newCard);
+
+        if (checkIfDealerLost() == true) {
+            gameResult = "Dealer lost";
+        }
+
+        return newCard.getValue() + "\nof\n" + newCard.getType() + "s";
+    }
+
+    public List<String> dealerDrawCards() {
+        List<String> cardList = new ArrayList<>();
+        List<Integer> dealerPoints = countPoints(dealerCards);
+        int maxDealerPoints = dealerPoints.get(dealerPoints.size() - 1);
+
+        while (maxDealerPoints < 17) {
+            Card newCard = cardPack.pickCard();
+            dealerCards.add(newCard);
+            //addCardToScene(newCard.getValue() + "\nof\n" + newCard.getType(), dealerCardContainer);
+            cardList.add(newCard.getValue() + "\nof\n" + newCard.getType());
+
+            maxDealerPoints = countPoints(dealerCards).get(dealerPoints.size() - 1);
+        }
+
+        return cardList;
+    }
+
+    public void calculateWinner() {
+        int maxDealerPoints = -1;
+        for (int pointValue : countPoints(dealerCards)) {
+            if (pointValue > maxDealerPoints && pointValue <= 21 || maxDealerPoints == -1)
+                maxDealerPoints = pointValue;
+        }
+
+        int maxPlayerPoints = -1;
+        for (int pointValue : countPoints(playerCards)) {
+            if (pointValue > maxPlayerPoints && pointValue <= 21  || maxPlayerPoints == -1)
+                maxPlayerPoints = pointValue;
+        }
+
+
+        if (maxDealerPoints == maxPlayerPoints) {
+            System.out.println("DRAW! " + maxDealerPoints + " = " + maxPlayerPoints);
+            gameResult = "Draw";
+        } else if (21 - maxDealerPoints < 21 - maxPlayerPoints && maxDealerPoints <= 21) {
+            System.out.println("Dealer won! " + maxDealerPoints + " > " + maxPlayerPoints);
+            gameResult = "Dealer won";
+        } else {
+            System.out.println("Player won! " + maxDealerPoints + " < " + maxPlayerPoints);
+            gameResult = "Player won";
+        }
+    }
+}
